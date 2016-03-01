e186e5123cfcb579a4598d770889f1179afcd60f
Dylan Blake <dm_blake@live.com>
2016-03-01 15:19:56 -0800

    server skeleton complete

--- server.cpp
+++ server.cpp
@@ -30,9 +30,15 @@ int main(int argc, char * argv[])
 {
     // init structures
     int listen_socket, client_len, port, sockopt_arg, max_descriptors, max_clients;
+    int new_socket, sockfd;
+    int nready;
     int client[FD_SETSIZE]; // array to hold clients
     int i; // loop variable
     struct sockaddr_in server, client_addr;
+    char *bp, buf[BUFLEN];
+    int n;
+    fd_set allset, rset;
+    bp = (char*)malloc(sizeof(char) * BUFLEN); // MAKE SURE YOU FREE THIS
     // set variables based on command line
     switch (argc)
     {
@@ -77,10 +83,119 @@ int main(int argc, char * argv[])
     {
         client[i] = -1; // -1 means available client
     }
-    // use select to determine if a socket has activity
-    // create new client socket
-    // handle receiving from a client
-    // handle sending to all clients
+    FD_ZERO(&allset); // clear the set
+    FD_SET(listen_socket, &allset); // add the listen socket to the set
+    while (1)
+    {
+        rset = allset;
+        // use select to determine if a socket has activity
+        nready = select(max_descriptors + 1, &rset, NULL, NULL, NULL);
+        // create new client socket
+        if (FD_ISSET(listen_socket, &rset))
+        {
+            client_len = sizeof(client_addr);
+            if ((new_socket = accept(listen_socket, (struct sockaddr *) &client_addr, (socklen_t *)&client_len)) == -1)
+            {
+                perror("Accept error");
+                exit(1);
+            }
+            for (i = 0; i < FD_SETSIZE; i++)
+            {
+                client[i] = new_socket;
+                break;
+            }
+            if (i == FD_SETSIZE)
+            {
+                fprintf(stderr, "Too many clients.\n");
+                exit(1);
+            }
+            FD_SET(new_socket, &allset);
+            max_descriptors = (new_socket > max_descriptors) ? new_socket : max_descriptors;
+            max_clients = (i > max_clients) ? i : max_clients;
+            if (--nready <= 0)
+            {
+                continue;
+            }
+            // handle receiving from a client
+            for (i = 0; i <= max_clients; i++)
+            {
+                // check for empty socket
+                if ((sockfd = client[i]) < 0)
+                {
+                    continue;
+                }
+                if (FD_ISSET(sockfd, &rset))
+                {
+                    n = read_from_socket(sockfd, bp, BUFLEN);
+                    // handle sending to all clients
+                    write_to_clients(buf, BUFLEN, client, max_clients, i);
+                    if (n == 0)
+                    {
+                        close(sockfd);
+                        FD_CLR(sockfd, &allset);
+                        client[i] = -1;
+                    }
+                    if (--nready <= 0)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
     // free resources
+    free(bp);
     return 0;
+}
+
+/*
+ * Function: write_to_clients
+ * Date: March 01, 2016
+ * Revision: v1
+ * Designer: Dylan
+ * Programmer: Dylan
+ * Interface: void write_to_clients(char *buf, int bufsize, int *clients, int num_clients, int sender)
+ *              buf : buffer of message to send
+ *              bufsize : max length of the buffer,
+ *              clients : pointer to array of client sockets
+ *              num_clients : max number of clients to send to
+ *              sender : the client that sent the original message
+ * Returns: void
+ * Notes:
+ *  Modifies the message in buf to include information such as 
+ *  username and timestamp, then sends that modified message
+ *  to each client EXCEPT the client identifed as the sender.
+ */
+void write_to_clients(char *buf, int bufsize, int *clients, int num_clients, int sender)
+{
+}
+
+/*
+ * Function: read_from_socket
+ * Date: March 01, 2016
+ * Revision: v1
+ * Designer: Dylan
+ * Programmer: Dylan
+ * Interface: int read_from_socket(int socket, char *buf, int bufsize)
+ *              socket : socket to read from
+ *              buf : buffer to read into
+ *              bufsize : size of the buffer or max size to read
+ * Returns: number of characters read
+ * Notes:
+ *  reads from the specified buffer up to bufsize characters.
+ *  It is the caller's responsibility to ensure that bufsize does
+ *  not overflow the buffer.
+ */
+int read_from_socket(int socket, char *buf, int bufsize)
+{
+    int n;
+    int total_bytes_read = 0;
+    int bytes_to_read = bufsize;
+    while ((n = read(socket, buf, bytes_to_read)) > 0)
+    {
+        buf += n; // advance the buffer by the amount read
+        total_bytes_read +=n;
+        bytes_to_read -= n;
+    }
+    return total_bytes_read;
 }
--- server.h
+++ server.h
@@ -6,6 +6,12 @@
 #include "string.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include <unistd.h>
 
 #define DEFAULT_PORT 4981
+#define BUFLEN 4096
 #define MAX_CONN 10
+
+int main(int argc, char* argv[]);
+int read_from_socket(int socket, char *buf, int bufsize);
+void write_to_clients(char *buf, int bufsize, int *clients, int num_clients, int sender);
